f1efe6e700e3e7eb07773652843dd92faa996897
mali295 <mali295@uwo.ca>
2022-12-26 17:01:25 -0500

    Set up nextjs project

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Flex, Box, Text, Button } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
