7c706404c47bb8b3a6ee34431d6068a92531a547
mali295 <mali295@uwo.ca>
2022-12-29 10:18:53 -0500

    Fetch the properties and map over them

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+  export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Key': 'ca9b1d44fcmsha5707d398f99d36p1802b2jsnfe0aed783e5c',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+          }
+    })
+  }
