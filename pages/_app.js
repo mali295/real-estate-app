e173dc0a1c31c9cdc602e772a71a5a560694529d
mali295 <mali295@uwo.ca>
2022-12-29 11:44:09 -0500

    Finish real estate properties for home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import Router from 'next/router'
+import Head from 'next/head'
+import NProgress from 'nprogress'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }) {
+import Layout from '../components/Layout'
+
+
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 )
 
 export default function Home(propertiesForSale, propertiesForRent) {
-  console.log(propertiesForSale, propertiesForRent)
   return (
     <Box>
       <Banner
@@ -47,8 +46,9 @@ export default function Home(propertiesForSale, propertiesForRent) {
         linkName="/search?purpose=for-sale"
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
+      <Flex flexWrap="wrap">
         {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
-
+      </Flex>
     </Box>
   )
 }
