ffae7c02b729e3931340db6c60b113de9d904902
mali295 <mali295@uwo.ca>
2022-12-29 10:44:12 -0500

    Added assets and imported images

--- components/Property.js
+++ components/Property.js
@@ -6,9 +6,15 @@ import { BsGridFill } from 'react-icons/bs'
 import { GoVerified } from 'react-icons/go'
 import millify from 'millify'
 
+import DefaultImage from '../assets/images/house.jpg'
+
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId} }) => (
     <Link href={`/property/${externalId}`} passHref>
-        {title} 
+        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+            <Box>
+                <Image src= {coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house" />
+            </Box>
+        </Flex> 
     </Link>
 )
 
