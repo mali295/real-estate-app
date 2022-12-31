f18545d7705240c5d622cb82fd4302ca1a8cfc3b
mali295 <mali295@uwo.ca>
2022-12-31 10:53:54 -0500

    Fetch data for search filter

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,13 @@
+import { usEffect, useState } from 'react'
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from "@chakra-ui/react"
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+import Image from 'next/image'
+
+const SearchFilters = () => {
+    <div>
+        searchFilters
+    </div>
+}
+
+export default SearchFilters
