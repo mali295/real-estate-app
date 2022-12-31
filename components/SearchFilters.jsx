2a77f9c68eb02e472b06625dc3bc0f33500481aa
mali295 <mali295@uwo.ca>
2022-12-31 11:13:29 -0500

    Complete search filters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,10 +4,31 @@ import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
 
+import {filterData, getFiltersValues } from '../utils/filterData'
+
 const SearchFilters = () => {
-    <div>
-        searchFilters
-    </div>
+    const [filters, setFilters] = useState(filterData)
+
+    const searchProperties = (filterValues) => {
+
+    }
+
+    return (
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select 
+                    placeholder={filter.placeholder}
+                    w="fit-content"
+                    p="2"
+                    onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
+                    >
+
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
+    )
 }
 
 export default SearchFilters
