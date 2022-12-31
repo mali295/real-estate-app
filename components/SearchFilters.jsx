a1d665fb997dccec9d29088804e8a46312ed33b1
mali295 <mali295@uwo.ca>
2022-12-31 11:27:57 -0500

    Create property details page

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,13 +4,22 @@ import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
 
-import {filterData, getFiltersValues } from '../utils/filterData'
+import {filterData, getFilterValues } from '../utils/filterData'
 
 const SearchFilters = () => {
     const [filters, setFilters] = useState(filterData)
 
     const searchProperties = (filterValues) => {
+        const path = router.pathname
+        const { query } = router
+        
+        const values = getFilterValues(filterValues)
 
+        values.forEach((item) => {
+            query[item.name] = item.value
+        })
+
+        router.push({ pathname: path, query})
     }
 
     return (
@@ -18,11 +27,17 @@ const SearchFilters = () => {
             {filters.map((filter) => (
                 <Box key={filter.queryName}>
                     <Select 
-                    placeholder={filter.placeholder}
-                    w="fit-content"
-                    p="2"
-                    onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
                     >
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            
+                            </option>
+                        ))}
 
                     </Select>
                 </Box>
